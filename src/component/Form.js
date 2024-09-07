462edbc9889408146e5bc831ad943518cabd2fbb
moaz ahmed <moaz44ahmed444@gmail.com>
2024-09-08 00:31:22 +0300

    edits

--- src/component/Form.js
+++ src/component/Form.js
@@ -3,9 +3,9 @@ import React from "react";
 const Form = (props) => {
     return(
         <form onSubmit={props.getPrays}>
-            <input type='text' name="city" placeholder="city ..."/>
-            <input type='text' name="country" placeholder="country ..."/>
-            <input type='date' name="date" className="custom-date-input" placeholder="date 'DD/MM/>YYYY' "/>
+            <input type='text' name="city" placeholder="city ..." required/>
+            <input type='text' name="country" placeholder="country ..." required/>
+            <input type='date' name="date" className="custom-date-input" required placeholder="date 'DD/MM/>YYYY' "/>
             <button >عرض مواقيت الصلاة</button>
         </form>
     )
